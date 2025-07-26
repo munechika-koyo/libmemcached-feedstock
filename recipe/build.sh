d1dbc5a7ef735c286d909b14d6ad889cb857028f
munechika-koyo <munechika.koyo@gmail.com>
2025-07-27 00:23:17 +0200

    Add command to copy updated config.sub and config.guess files

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,9 @@
 set -e
 set -x
 
+# Get an updated config.sub and config.guess
+cp $BUILD_PREFIX/share/libtool/build-aux/config.* .
+
 ./configure --prefix="$PREFIX"
 
 make -j${CPU_COUNT} ${VERBOSE_AT}
