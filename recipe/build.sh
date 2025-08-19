dd3e66732af3c858b2bccccd2e3199fa84ac960e
munechika-koyo <munechika.koyo@gmail.com>
2025-08-19 07:53:35 +0200

    Fix what shellcheck suggested

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,11 +3,11 @@ set -e
 set -x
 
 # Get an updated config.sub and config.guess
-cp $BUILD_PREFIX/share/gnuconfig/config.* ./build-aux
+cp "${BUILD_PREFIX}"/share/gnuconfig/config.* ./build-aux
 
-./configure --prefix=${PREFIX}
+./configure --prefix="${PREFIX}"
 
-make ${VERBOSE_AT}
+make "${VERBOSE_AT}"
 ## tests hang on `testapp` on osx and linux
 # make check
 make install
